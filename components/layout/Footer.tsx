d7a9bbedc8ae432b093a25d579e90c5f55327fe3
lucas1288 <jc-stora@protonmail.com>
2026-03-30 12:52:40 +0200

    fixed footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import type { SocialLink } from "./types";
 import styles from "./Footer.module.scss";
+import { useIsDesktop } from "@/hooks/useWindowWidth";
+import SocialLinks from "../common/SocialLinks";
 
 interface FooterProps {
   footerText: string;
@@ -7,20 +11,25 @@ interface FooterProps {
 }
 
 const Footer = ({ footerText, socialLinks }: FooterProps) => {
+  const isDesktop = useIsDesktop();
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p>{footerText}</p>
         <div className={styles.socialLinks}>
-          {socialLinks.map((link) => (
-            <a
-              key={`${link.platform}-${link.url}`}
-              href={link.url}
-              aria-label={link.label}
-            >
-              {link.label}
-            </a>
-          ))}
+          {isDesktop ? (
+            socialLinks.map((link) => (
+              <a
+                key={`${link.platform}-${link.url}`}
+                href={link.url}
+                aria-label={link.label}
+              >
+                {link.label}
+              </a>
+            ))
+          ) : (
+            <SocialLinks />
+          )}
         </div>
       </div>
     </footer>
